95cc182a40d919b4b4488b39b8eefe0dddbc7d31
SHIMOMURA Sho <graueule@gmail.com>
2013-05-29 17:59:23 +0900

    tmp

--- js/app.js
+++ js/app.js
@@ -1,19 +1,65 @@
-App = Ember.Application.create();
-
-App.Router.map(function() {
-    this.resource("about");
-});
-
-App.IndexRoute = Ember.Route.extend({
-});
-
-
+var App = Ember.Application.create();
 var bg = chrome.extension.getBackgroundPage();
-console.log(bg.rooms);
 
+//------------------------------------------------------------
+// model declaration
+//------------------------------------------------------------
+App.Store = DS.Store.extend({
+  revision: 12,
+  adapter: "DS.FixtureAdapter"
+});
+
+App.Room = DS.Model.extend({
+    user: DS.belongsTo('App.User'),
+    members: DS.hasMany('App.User'),
+    messages: DS.hasMany('App.Message'),
+    name: DS.attr('string'),
+    nickname: DS.attr('string'),
+    updated_at: DS.attr('date'),
+});
+App.User = DS.Model.extend({
+    room: DS.belongsTo('App.Room'),
+    name: DS.attr('string'),
+    profile_image_url: DS.attr('string'),
+    screen_name: DS.attr('string'),
+});
+App.Message = DS.Model.extend({
+    room: DS.belongsTo('App.Room'),
+    body: DS.attr('string'),
+    created_at: DS.attr('date'),
+    name: DS.attr('string'),
+    profile_image_url: DS.attr('string'),
+    screen_name: DS.attr('string'),
+    view: DS.attr('string'),
+});
+
+// model setup
+App.Room.FIXTURES = bg.rooms;
+App.Message.FIXTURES = bg.messages;
+
+//------------------------------------------------------------
+// routing set up
+//------------------------------------------------------------
+App.Router.map(function() {
+    this.route("rooms", { path: "/" });
+    this.resource("rooms");
+    this.resource("room", {path:"/room/:room_id"});
+    this.resource("setting");
+});
+App.RoomsRoute = Ember.Route.extend({
+    model: function(){
+        return App.Room.find();
+    }
+});
+
+//------------------------------------------------------------
+// callbacks
+//------------------------------------------------------------
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     if(request.AsakusaSatelliteUpdate) {
-        console.log("update "+request.AsakusaSatelliteUpdate);
-        console.log(eval("bg." + request.AsakusaSatelliteUpdate));
+//        App.Message.FIXTURES = bg.messages;
+
+//        console.log("update "+request.AsakusaSatelliteUpdate);
+//        console.log(eval("bg." + request.AsakusaSatelliteUpdate));
     }
 });
--- js/background.js
+++ js/background.js
@@ -4,27 +4,28 @@ var token = "yTzvPnmThlRBCS0udKyEliEijJ2mR"
 chrome.browserAction.setBadgeBackgroundColor({color:[190, 190, 190, 230]});
 
 var rooms = [];
-var messages = {};
+var messages = [];
 
 var data = {auth_token:token};
 $.get(url + "/api/v1/room/list.json", data, function(json){
     rooms = json;
-    rooms = rooms.slice(0,1);
 
     var counter = 0;
     $.each(rooms, function(idx, room){
         var data = $.extend(data, {room_id: room.id});
         $.get(url + "/api/v1/message/list.json", data, function(json){
-console.log(json);
-            messages[room.id] = json;
+            //room.messages = json;
+            $.map(json, function(message, idx){
+                message.room = message.room.id;
+            });
+            messages = messages.concat(json);
+            
             counter += json.length;
 
             chrome.browserAction.setBadgeText({text:""+counter});
 
-            var update = "messages['"+room.id+"']";
-setInterval(function(){
+            var update = "messages";
             chrome.runtime.sendMessage({AsakusaSatelliteUpdate:update});
-}, 3000);
         });
     });
 });
