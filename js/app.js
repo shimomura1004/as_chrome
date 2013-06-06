10b84226493e87015d3507387caaf738a1444f34
SHIMOMURA Sho <graueule@gmail.com>
2013-06-06 10:49:12 +0900

    remove ember-data and modify style

--- js/app.js
+++ js/app.js
@@ -2,38 +2,6 @@ var App = Ember.Application.create();
 var bg = chrome.extension.getBackgroundPage();
 
 //------------------------------------------------------------
-// model declaration
-//------------------------------------------------------------
-App.Store = DS.Store.extend({
-  revision: 12,
-  adapter: "DS.FixtureAdapter"
-});
-
-App.Room = DS.Model.extend({
-    user: DS.belongsTo('App.User'),
-    members: DS.hasMany('App.User'),
-    messages: DS.hasMany('App.Message'),
-    name: DS.attr('string'),
-    nickname: DS.attr('string'),
-    updated_at: DS.attr('date'),
-});
-App.User = DS.Model.extend({
-    room: DS.belongsTo('App.Room'),
-    name: DS.attr('string'),
-    profile_image_url: DS.attr('string'),
-    screen_name: DS.attr('string'),
-});
-App.Message = DS.Model.extend({
-    room: DS.belongsTo('App.Room'),
-    body: DS.attr('string'),
-    created_at: DS.attr('string'),
-    name: DS.attr('string'),
-    profile_image_url: DS.attr('string'),
-    screen_name: DS.attr('string'),
-    view: DS.attr('string'),
-});
-
-//------------------------------------------------------------
 // routing set up
 //------------------------------------------------------------
 App.Router.map(function() {
